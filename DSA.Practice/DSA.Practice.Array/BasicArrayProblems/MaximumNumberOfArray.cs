098127410270e05174bc372cb5c89d233ca18959
chayandutta <chayan.dutta@mindteck.com>
2025-06-13 22:17:19 +0530

    Projects and basic array operation

---
+++ DSA.Practice/DSA.Practice.Array/BasicArrayProblems/MaximumNumberOfArray.cs
@@ -0,0 +1,27 @@
+﻿namespace DSA.Practice.Array.BasicArrayProblems;
+
+/// <summary>
+/// 
+/// </summary>
+/// <param name="myArray"></param>
+public class MaximumNumberOfArray(int[] myArray)
+{
+    private readonly int[] myArray = myArray;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    public void FindMaximumNumberOfArray()
+    {
+        int maximum = 0;
+
+        for (int i = 0; i < myArray.Length; i++)
+        {
+            if (myArray[i] > maximum) 
+                maximum = myArray[i]; 
+        }
+
+        Console.WriteLine("The maximum number in this array is - {0}", maximum);
+    }
+}
---
+++ DSA.Practice/DSA.Practice.Array/Program.cs
@@ -0,0 +1,8 @@
+﻿using DSA.Practice.Array.BasicArrayProblems;
+
+Console.WriteLine("-  Array Practice    -");
+
+int[] array1 = { 1, -1, 0, 55, 10 };
+
+MaximumNumberOfArray maximumNumberOfArray = new MaximumNumberOfArray(array1);
+maximumNumberOfArray.FindMaximumNumberOfArray();
