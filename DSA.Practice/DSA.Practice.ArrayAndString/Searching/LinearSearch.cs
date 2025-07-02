2948a482094f086fd51be1e89edceaf44371ce47
chayandutta <chayan.dutta@mindteck.com>
2025-07-02 23:51:24 +0530

    Linera search algo

---
+++ DSA.Practice/DSA.Practice.ArrayAndString/Searching/LinearSearch.cs
@@ -0,0 +1,31 @@
+﻿namespace DSA.Practice.ArrayAndString.Searching;
+
+public class LinearSearch
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="array"></param>
+    /// <param name="element"></param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentException"></exception>
+    public static int SearchElementByLinearSearch<T>(T[] array, T element)
+    {
+       int n = array.Length;
+
+        if (n == 0 || array == null)
+            throw new ArgumentException("Array is empty or null", nameof(array));
+
+        if (element == null)
+            throw new ArgumentException("element is null", nameof(element));
+
+        for (int i = 0; i < n; i++)
+        {
+            if (array[i]!.Equals(element))
+                return i + 1;
+        }
+
+        return -1;
+    }
+}
